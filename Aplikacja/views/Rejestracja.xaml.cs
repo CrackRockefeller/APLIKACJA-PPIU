50257d34eca4cd621e8a98eca7001c0c6d29822e
Michał Pieczka <33464181+Kortisss@users.noreply.github.com>
2021-05-21 15:37:20 +0200

    powierdzenie maila
    Dodałem wymóg potwierdzenia adresu email. Teraz trzeba wpisać kod który dostajecie na wpisanego w textboxa maila.

--- Aplikacja/views/Rejestracja.xaml.cs
+++ Aplikacja/views/Rejestracja.xaml.cs
@@ -20,11 +20,23 @@ using FluentValidation.Results;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Threading;
+using System.Threading.Tasks;
+
+using MailKit.Net.Smtp;
+using MailKit;
+using MimeKit;
 
 namespace Aplikacja
 {
     public sealed partial class Rejestracja : Page
     {
+        
+        private bool wyslano = false;
+        public static Random rnd = new Random();
+        int a = rnd.Next(0, 10000);
+
+        Uzytkownicy uzytkownik = new Uzytkownicy();
+
         public Rejestracja()
         {
             this.InitializeComponent();
@@ -34,7 +46,9 @@ namespace Aplikacja
         {
             BindingList<string> errList = new BindingList<string>();
 
-            Uzytkownicy uzytkownik = new Uzytkownicy();
+            
+
+            
             uzytkownik.email = email.Text;
             uzytkownik.imie = imie.Text;
             uzytkownik.haslo = haslo.Password;
@@ -58,15 +72,69 @@ namespace Aplikacja
             //tu dodaj do bazy
             if (errList.Count == 0)
             {
-                DataAccess.dodajUzytkownika(uzytkownik.email, uzytkownik.imie, uzytkownik.haslo);
+                if (!StandardPopup.IsOpen) StandardPopup.IsOpen = true;
+                
+                
+                
+
+                var message = new MimeMessage();
+                message.From.Add(new MailboxAddress("uwpApplication", "uwpapplication.uwpapplication@interia.pl"));
+                message.To.Add(new MailboxAddress(uzytkownik.imie, uzytkownik.email));
+                message.Subject = "Kod aktywacyjny konto UWP; Aplikacja finansowa";
+
+                string txt = @"Dziękujemy za rejestrację,<br>
+                               <p>Wpisz ten kod do swojej aplikacji aby założyć konto:</p><p>" + a.ToString()+ @"</p><br>
+                               <p>-- UWPApplication</p>";
+
+                message.Body = new TextPart("Html")
+                {
+                    Text = txt
+                };
+
+                using (var client = new SmtpClient())
+                {
+                    client.Connect("poczta.interia.pl", 587, false);
+                    
+                    // Note: only needed if the SMTP server requires authentication
+                    client.Authenticate("uwpapplication.uwpapplication@interia.pl", "1234567Mm.");
+                    Debug.WriteLine("The mail has been sent successfully !!");
+                    client.Send(message);
+                    client.Disconnect(true);
+
+                    wyslano = true;
+                }
+                //DataAccess.dodajUzytkownika(uzytkownik.email, uzytkownik.imie, uzytkownik.haslo);
                 //Thread.Sleep(2000);
-                Frame.GoBack();
+                //Frame.GoBack();
             }
         }
+
+
 
         private void btnPowrot_Click(object sender, RoutedEventArgs e)
         {
             Frame.GoBack();
+        }
+
+        private void potwierdz_Click(object sender, RoutedEventArgs e)
+        {
+            if (wyslano)
+            {
+                if (kodTextBox.Text == a.ToString())
+                {
+                    Debug.WriteLine("kody się zgadzają!!!");
+                    DataAccess.dodajUzytkownika(uzytkownik.email, uzytkownik.imie, uzytkownik.haslo);
+                    Debug.WriteLine("poprawnie zarejestrowano użytkownika!");
+                    wyslano = false;
+                    Frame.GoBack();
+                }
+                else
+                {
+                    Debug.WriteLine("zły kod z maila");
+                }
+                
+            }
+            
         }
     }
 }

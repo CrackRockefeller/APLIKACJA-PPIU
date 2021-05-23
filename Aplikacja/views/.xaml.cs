7f8e78be0f36f3888bb73f16e95487dd00d6b415
TaktyKK1 <80386285+TaktyKK1@users.noreply.github.com>
2021-05-24 00:42:11 +0200

    Testy
    Jakies dodawanie do bazy wydatku, ale jeszcze bez zdjecia, i wybieranie z bazy danych do listy takie testowe na razie.

--- Aplikacja/views/.xaml.cs
+++ Aplikacja/views/.xaml.cs
@@ -31,9 +31,49 @@ namespace Aplikacja
             this.InitializeComponent();
             test.Text = localSettings.Values["loggedUser"].ToString();
         }
-        private void AddData(object sender, RoutedEventArgs e)
+        private void btnDodajWpis(object sender, RoutedEventArgs e)
         {
-            Output.ItemsSource = DataAccess.GetData();
+            int idUzytkownika = Convert.ToInt32(DataAccess.sprawdzUzytkownika(test.Text));
+            String kwotaWpisana = txtKwota.Text.Replace(",", ".");
+            if (kwotaWpisana != "")
+            {
+
+                if (DatePickerData.SelectedDate != null)
+                {
+                    string data = DatePickerData.Date.ToString("yyyy-MM-dd");
+                    double kwota = Convert.ToDouble(kwotaWpisana);
+                    DataAccess.dodajWpisUzytkownika(kwota, txtOpis.Text, data, idUzytkownika, Convert.ToString(comboBoxTypWydatku.SelectedItem)); //trzeba jeszcze jakos zdjecie dodawac 
+                }
+                else
+                {
+                    test2.Text = "Wprowadz jakas date!";
+                }
+            }
+            else
+            {
+                test2.Text = "Wprowadz jakas kwote!";
+            }
         }
+<<<<<<< Updated upstream
+=======
+
+        private void btnCofnij(object sender, RoutedEventArgs e)
+        {
+            Frame.GoBack();
+        }
+
+        private void txtKwota_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            double kwotaWpisana = 0;
+            try
+            {
+                kwotaWpisana = Convert.ToDouble(txtKwota.Text);
+            }
+            catch
+            {
+                txtKwota.Text = "";
+            }
+        }
+>>>>>>> Stashed changes
     }
 }
